c5f96869c39e4e47f642809bec3c0d8c59edb758
Akshay Narayanan Balajee Viswanath <akshayna001@e.ntu.edu.sg>
2025-04-03 11:50:56 +0800

    Create Query6.sql

---
+++ Query/Query6.sql
@@ -0,0 +1,35 @@
+WITH InvestorAge AS (
+ SELECT
+ i.Phone,
+ i.Name,
+ i.Company,
+ i.DoB,
+ DATEDIFF(YEAR, i.DoB, GETDATE()) AS Age
+ FROM
+ INVESTOR i
+),
+AgeFiltered AS (
+ SELECT
+ ia.Phone,
+ ia.Name,
+ ia.Company,
+ ia.Age
+ FROM
+ InvestorAge ia
+ WHERE
+ ia.Age BETWEEN 30 AND 40
+)
+SELECT
+ af.Company,
+ fg.Goal,
+ COUNT(*) AS NumberOfInvestors
+FROM
+ AgeFiltered af
+JOIN
+ FINANCIAL_GOAL fg ON af.Phone = fg.Phone
+GROUP BY
+ af.Company,
+ fg.Goal
+ORDER BY
+ af.Company,
+ NumberOfInvestors DESC;
